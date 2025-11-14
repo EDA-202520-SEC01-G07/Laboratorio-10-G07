a30bdc636c587e133753b341d218a3fbe172e520
jlozadab <j.lozadab@uniandes.edu.co>
2025-11-14 07:38:00 -0500

    insert vrtx

--- DataStructures/Graph/digraph.py
+++ DataStructures/Graph/digraph.py
@@ -1,8 +1,17 @@
 from DataStructures.List import array_list as lt
 from DataStructures.Map import map_linear_probing as ml
 def new_graph(order):
-    """
-    Crea un grafo dirigido.
-    order: Numero de vertices del grafo
-    """
     graph = ml.new_map(order, 0.5, prime=109345121)
+    return graph
+    
+def insert_vertex(my_graph, key_u, info_u):
+    """
+    Inserta un vertice en el grafo dirigido.
+    my_graph: Grafo dirigido
+    key_u: Llave del vertice
+    info_u: Informacion del vertice
+    """
+    ml.put(my_graph["vertices"], key_u, info_u)
+    my_graph["order"] += 1
+    return my_graph
+
