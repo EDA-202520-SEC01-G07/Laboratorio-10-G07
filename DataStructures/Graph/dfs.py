7cc4081e50982d2f2fc62a4fa77b83b6f0b68704
MClaraQ <m.quijanoa@uniandes.edu.co>
2025-11-20 19:18:34 -0500

    path_to

--- DataStructures/Graph/dfs.py
+++ DataStructures/Graph/dfs.py
@@ -1,6 +1,8 @@
 from DataStructures.Graph import digraph as G
 from DataStructures.List import array_list as lt
 from DataStructures.Queue import queue as q
+from DataStructures.Stack import stack as s
+
 def dfs(my_graph, source):
     visitados= G.new_graph(order=0)
     dfs_vertex(my_graph, source, visitados)
@@ -31,4 +33,14 @@ def dfs_vertex(my_graph, vertex_key, visitados):
 def has_path_to(key_v, visited_map):
     return G.contains_vertex(visited_map, key_v)
 
-# Hacer el path_to
+def path_to(key_v, visited_map):
+    if not has_path_to(key_v, visited_map):
+        return None
+    else:
+        path=s.new_stack()
+        current_key=key_v
+        while current_key is not None:
+            s.push(path, current_key)
+            current_key=visited_map[current_key]
+        return path
+    
