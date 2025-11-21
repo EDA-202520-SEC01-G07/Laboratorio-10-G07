723870e4dd93a0b9b7a54d0631dd800f8f77f590
jlozadab <j.lozadab@uniandes.edu.co>
2025-11-21 07:09:31 -0500

    bfs,bfs_vertex

--- DataStructures/Graph/bfs.py
+++ DataStructures/Graph/bfs.py
@@ -4,14 +4,8 @@ from DataStructures.Queue import queue as q
 from DataStructures.Stack import stack as s
 from DataStructures.Map import map_linear_probing as ml
 def bfs(my_graph, source):
-    visited_map = ml.new_map(
-        num_elements=G.order(my_graph),
-        load_factor=0.5
-    )
-    ml.put(visited_map, source, {
-        "edge_from": None,
-        "dist_to": 0
-    })
+    visited_map = ml.new_map(num_elements=G.order(my_graph), load_factor=0.5, prime=109345121)
+    ml.put(visited_map, source, {"edge_from": None,"dist_to": 0})
     bfs_vertex(my_graph, source, visited_map)
     return visited_map
 
@@ -23,17 +17,12 @@ def bfs_vertex(my_graph, source, visited_map):
     while not q.is_empty(cola):
         v = q.dequeue(cola)
         adj = G.adjacent(my_graph, v)
-
         for i in range(lt.size(adj)):
             w = lt.get_element(adj, i)
             if not ml.contains(visited_map, w):
                 info_v = ml.get(visited_map, v)
-                ml.put(visited_map, w, {
-                    "edge_from": v,
-                    "dist_to": info_v["dist_to"] + 1
-                })
+                ml.put(visited_map, w, {"edge_from": v,"dist_to": info_v["dist_to"] + 1})
                 q.enqueue(cola, w)
-
     return visited_map
 
 
